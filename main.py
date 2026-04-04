4b8c044abbe0e62566b91aebb54dc7067a344497
Abdelrahman <129596425+abdrhmnr@users.noreply.github.com>
2026-04-04 23:18:54 +0200

    Refactor main.py and base.py for improved structure; update requirements.txt for dependency versions; add .vscode/settings.json for Python REPL configuration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from email.mime import base
-
-from fastapi import FastAPI
 from routes.base import base_router
+from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
 
 app = FastAPI()
 
--- routes/base.py
+++ routes/base.py
@@ -1,9 +1,17 @@
+
 from fastapi import FastAPI, APIRouter
+import os
 
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["base"]
+)
 
 
 @base_router.get("/")
-def welcome():
-    return {"message": "Welcome to mini-rag!"}
+async def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    return {"app_name": app_name,
+            "app_version": app_version}
